ab38acdfbd740ebd2b21442eec1a4ffd0babceed
Kadrick <kbk2581553@gmail.com>
2021-07-28 14:33:34 +0900

    solve 2 problem

--- BOJ/template.cpp
+++ BOJ/template.cpp
@@ -1,11 +1,12 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define fastio ios::sync_with_stdio(false); cin.tie(0);
+#define fastio                                                                 \
+  ios::sync_with_stdio(false);                                                 \
+  cin.tie(0);
 #define endl '\n'
 
-int main(void)
-{
-    fastio
+int main(void) {
+  fastio;
 
-    return 0;
+  return 0;
 }
