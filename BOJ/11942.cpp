f8388101f2d00c1da89ea981a1ea7ff35c31b008
Kadrick <kbk2581553@gmail.com>
2021-08-24 02:47:27 +0900

    solve 4 problem

---
+++ BOJ/11942.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define fastio                                                                 \
+  ios::sync_with_stdio(false);                                                 \
+  cin.tie(0);
+#define endl '\n'
+
+int main(void) {
+  fastio;
+
+  cout << "고려대학교";
+
+  return 0;
+}
---
+++ BOJ/14645.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define fastio                                                                 \
+  ios::sync_with_stdio(false);                                                 \
+  cin.tie(0);
+#define endl '\n'
+
+int main(void) {
+  fastio;
+
+  int n, k, tmp;
+  cin >> n >> k;
+  for (int i = 0; i < n; ++i) {
+    for (int j = 0; j < k; ++j) {
+      cin >> tmp;
+    }
+  }
+
+  cout << "비와이";
+
+  return 0;
+}
