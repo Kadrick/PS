600236772d021684f678d128d4fdfe1e76883d0a
Kadrick <kbk2581553@gmail.com>
2022-04-06 11:10:42 +0900

    add : template.go

---
+++ BOJ/template.go
@@ -0,0 +1,18 @@
+/**
+ * @file template.go
+ * @author kadrick (kbk2581553@gmail.com)
+ * @brief
+ * @version 0.1
+ * @date 2021-11-18 16:29
+ *
+ * @copyright Copyright (c) 2021
+ *
+ */
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("hello world")
+	return
+}
